4ebdcf29539642e801284db538d83ba15a824686
Pedro Cavalero <pedro.cavalero@ericsson.com>
2018-04-27 20:30:24 -0300

    Colocando as posicoes com um array

--- src/JogoDaVelhaMain.java
+++ src/JogoDaVelhaMain.java
@@ -3,15 +3,7 @@ import java.util.Scanner;
 public class JogoDaVelhaMain {
 
 	public static void main(String[] args) {
-		char posicao11=' ';
-		char posicao12=' ';
-		char posicao13=' ';
-		char posicao21=' ';
-		char posicao22=' ';
-		char posicao23=' ';
-		char posicao31=' ';
-		char posicao32=' ';
-		char posicao33=' ';
+		char[] posicoes = new char[9];
 		Scanner console = new Scanner(System.in);
 		System.out.println("Ola! Benvindo ao Jogo da Velha!"
 				+ "\nO X sempre comeca! O jogo termina se alguem ganhar ou der velha. ");
@@ -22,126 +14,48 @@ public class JogoDaVelhaMain {
 			do {
 				//Recebe a jogada atual
 				System.out.printf("\nInsira a posicao de %s (1 a 9) ", jogadorAtual);
-				String posicao = console.nextLine();
-				switch (posicao) {
-				case "1":
-					if(posicao11==' ') {
-						posicao11 = jogadorAtual;
-						jogadaOk=true;
-					} else {
-						jogadaOk=false;
-					}
-					break;
-				case "2":
-					if(posicao12==' ') {
-						posicao12 = jogadorAtual;
-						jogadaOk=true;
-					} else {
-						jogadaOk=false;
-					}
-					break;
-				case "3":
-					if(posicao13==' ') {
-						posicao13 = jogadorAtual;
-						jogadaOk=true;
-					} else {
-						jogadaOk=false;
-					}
-					break;
-				case "4":
-					if(posicao21==' ') {
-						posicao21 = jogadorAtual;
-						jogadaOk=true;
-					} else {
-						jogadaOk=false;
-					}
-					break;
-				case "5":
-					if(posicao22==' ') {
-						posicao22 = jogadorAtual;
-						jogadaOk=true;
-					} else {
-						jogadaOk=false;
-					}
-					break;
-				case "6":
-					if(posicao23==' ') {
-						posicao23 = jogadorAtual;
-						jogadaOk=true;
-					} else {
-						jogadaOk=false;
-					}
-					break;
-				case "7":
-					if(posicao31==' ') {
-						posicao31 = jogadorAtual;
-						jogadaOk=true;
-					} else {
-						jogadaOk=false;
-					}
-					break;
-				case "8":
-					if(posicao32==' ') {
-						posicao32 = jogadorAtual;
-						jogadaOk=true;
-					} else {
-						jogadaOk=false;
-					}
-					break;
-				case "9":
-					if(posicao33==' ') {
-						posicao33 = jogadorAtual;
-						jogadaOk=true;
-					} else {
-						jogadaOk=false;
-					}
-					break;
+				int posicao = console.nextInt();
+				if(posicoes[posicao]=='\u0000') {
+					posicoes[posicao] = jogadorAtual;
+					jogadaOk=true;
+				} else {
+					jogadaOk=false;
 				}
 				if(!jogadaOk) {
 					System.out.println("Jogada inválida! Escolha uma posicao vazia");
 				}
 			}while (!jogadaOk);
 			//imprime tabuleiro atual
-			System.out.println(
-					posicao11 + " | " + posicao12 + " |" + posicao13 +
-					"\n__________" +
-					"\n"+posicao21 + " | " + posicao22 + " |" + posicao23 +
-					"\n__________" +
-					"\n"+posicao31 + " | " + posicao32 + " |" + posicao33);
+			for(int j=0; j<posicoes.length; j+=3) {
+				System.out.println(posicoes[j]+" | "+posicoes[j+1]+" | "+posicoes[j+2]);
+			}
 			//verifica se alguem ganhou
-			if( (posicao11!=' ' && posicao11==posicao12 && posicao12==posicao13) ){
-				System.out.printf("Jogador %s ganhou!", posicao11);
-				break;
+			//linha
+			for (int j = 0; j < posicoes.length; j+=3) {
+				if(posicoes[j]!='\u0000' && posicoes[j]==posicoes[j+1] && posicoes[j+1]==posicoes[j+2]) {
+					System.out.printf("Jogador %s ganhou!", posicoes[j]);
+					return;
+				}
 			}
-			if(	(posicao21!=' ' && posicao21==posicao22 && posicao22==posicao23) ){
-				System.out.printf("Jogador %s ganhou!", posicao21);
-				break;
-			}
-			if(	(posicao31!=' ' && posicao31==posicao32 && posicao32==posicao33) ){
-				System.out.printf("Jogador %s ganhou!", posicao31);
-				break;
-			}
-			if(	(posicao11!=' ' && posicao11==posicao21 && posicao21==posicao31) ){
-				System.out.printf("Jogador %s ganhou!", posicao11);
-				break;
-			}
-			if(	(posicao12!=' ' && posicao12==posicao22 && posicao22==posicao32) ){
-				System.out.printf("Jogador %s ganhou!", posicao12);
-				break;
-			}
-			if(	(posicao13!=' ' && posicao13==posicao23 && posicao23==posicao33) ){
-				System.out.printf("Jogador %s ganhou!", posicao13);
-				break;
-			}
-			if(	(posicao11!=' ' && posicao11==posicao22 && posicao22==posicao33) ){
-				System.out.printf("Jogador %s ganhou!", posicao11);
-				break;
-			}
-			if(	(posicao13!=' ' && posicao13==posicao22 && posicao22==posicao31) ) {
-				System.out.printf("Jogador %s ganhou!", posicao13);
-				break;
+			//coluna
+			for (int j = 0; j < 3; j++) {
+				if(posicoes[j]!='\u0000' && posicoes[j]==posicoes[j+3] && posicoes[j+3]==posicoes[j+6]) {
+					System.out.printf("Jogador %s ganhou!", posicoes[j]);
+					return;
+				}
 			}
 
+			//diagonal principal
+			if(posicoes[0]!='\u0000' && posicoes[0]==posicoes[4] && posicoes[4]==posicoes[8]) {
+				System.out.printf("Jogador %s ganhou!", posicoes[0]);
+				return;
+			}
+			
+			//diagonal secundaria
+			if(posicoes[2]!='\u0000' && posicoes[2]==posicoes[4] && posicoes[4]==posicoes[5]) {
+				System.out.printf("Jogador %s ganhou!", posicoes[2]);
+				return;
+			}
 			//inverte jogador pra proxima rodada
 			jogadorAtual = (jogadorAtual=='X')?'O':'X';
 			if(i==8) {
